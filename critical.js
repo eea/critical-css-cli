7960740f5274b9644015fd5f116c2c4d1b3e37ff
nileshgulia1 <nileshgulia@gmail.com>
2021-01-15 15:33:43 +0530

    manually write file using node,don't write if less content received

--- critical.js
+++ critical.js
@@ -17,17 +17,12 @@ const generateCritical = async (url, size, location) => {
     rebase: false,
     minify: true,
     src: url,
-    target: {
-      css: __dirname + `${location}/critical.css`,
-    },
     penthouse: {
       timeout: 40000,
       pageLoadSkipTimeout: 30000,
     },
     dimensions,
   });
-
-  console.log(`Done. critical.css saved in ${location}/critical.css `);
   return css;
 };
 
--- index.js
+++ index.js
@@ -2,13 +2,36 @@
 
 "use strict";
 const generator = require("./critical");
+const path = require("path");
+const fs = require("fs");
+const makeDir = require("make-dir");
+const { promisify } = require("util");
+const writeFileAsync = promisify(fs.writeFile);
+
 const url = process.env.URL;
 const dimensions = process.env.DIMENSIONS || "1300x900";
 const location = process.env.LOCATION || "/output";
 
+async function outputFileAsync(file, data) {
+  const dir = path.join(__dirname, file);
+
+  if (!fs.existsSync(dir)) {
+    await makeDir(dir);
+  }
+
+  return writeFileAsync(`${dir}/critical.css`, data);
+}
+
 generator
   .generateCritical(url, dimensions, location)
-  .then((css) => console.log(css))
+  .then((css) => {
+    //console.log(css);
+    if (css.length > 10)
+      outputFileAsync(location, css).then(() =>
+        console.log(`Done. critical.css saved in ${location}/critical.css `)
+      );
+    else console.log("Invalid css generated! Please try with different URL");
+  })
   .catch((err) => {
     // console.error(err);
     console.error(err.name);
