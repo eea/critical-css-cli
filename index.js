477f515b0caa0030ddd3217ac8c41d5ef5e65797
Nilesh <nileshgulia@gmail.com>
2021-01-19 00:10:00 +0530

    adapt to using multiple url for css abstraction (#5)
    * adapt to using multiple urls
    
    * naming covention
    
    * cosmetics
    
    * WIP

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ const makeDir = require("make-dir");
 const { promisify } = require("util");
 const writeFileAsync = promisify(fs.writeFile);
 
-const url = process.env.URL;
+const urls = process.env.URL;
 const dimensions = process.env.DIMENSIONS || "1300x900";
 const location = process.env.LOCATION || "/output";
+const urlArray = urls.split(",");
+let counter = 1;
 
 async function outputFileAsync(file, data) {
   const dir = path.join(__dirname, file);
@@ -19,21 +21,32 @@ async function outputFileAsync(file, data) {
     await makeDir(dir);
   }
 
-  return writeFileAsync(`${dir}/critical.css`, data);
+  return writeFileAsync(`${dir}/critical-${counter}.css`, data);
 }
 
-generator
-  .generateCritical(url, dimensions, location)
-  .then((css) => {
-    console.log(css);
-    if (css.length > 10)
-      outputFileAsync(location, css).then(() =>
-        console.log(`Done. critical.css saved in ${location}/critical.css `)
-      );
-    else console.log("Invalid css generated! Please try with different URL");
-  })
-  .catch((err) => {
-    // console.error(err);
-    console.error(err.name);
-    console.error(err.message);
-  });
+const extractor = async () => {
+  const singleUrl = urlArray.pop();
+  if (!singleUrl) {
+    return Promise.resolve();
+  }
+  return generator
+    .generateCritical(singleUrl, dimensions, location)
+    .then(async (css) => {
+      if (css.length > 10) {
+        await outputFileAsync(location, css).then(() =>
+          console.log(
+            `Done. critical-${counter}.css saved in ${location}/critical-${counter}.css `
+          )
+        );
+        counter++;
+        return urlArray.length > 0 && extractor();
+      } else
+        console.log("Invalid css generated! Please try with different URL");
+    })
+    .catch((err) => {
+      // console.error(err);
+      console.error(err.name);
+      console.error(err.message);
+    });
+};
+extractor();
